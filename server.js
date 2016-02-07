44c59d732509a8812f998b712d752577ad56758c
Fabian Baier <fabian.baier1987@gmail.com>
2016-02-07 13:46:34 -0800

    added post to server.js

--- server.js
+++ server.js
@@ -17,13 +17,26 @@ router.get("/",function(req,res){
 
 router.route("/users")
     .get(function(req,res){
+        ------------------------------------------------------
+    })
+    .post(function(req,res){
+        var db = new mongoOp();
         var response = {};
-        mongoOp.find({},function(err,data){
-        // Mongo command to fetch all data from collection.
+        // fetch email and password from REST request.
+        // Add strict validation when you use this in Production.
+        db.userEmail = req.body.email; 
+        // Hash the password using SHA1 algorithm.
+        db.userPassword =  require('crypto')
+                          .createHash('sha1')
+                          .update(req.body.password)
+                          .digest('base64');
+        db.save(function(err){
+        // save() will run insert() command of MongoDB.
+        // it will add new data in collection.
             if(err) {
-                response = {"error" : true,"message" : "Error fetching data"};
+                response = {"error" : true,"message" : "Error adding data"};
             } else {
-                response = {"error" : false,"message" : data};
+                response = {"error" : false,"message" : "Data added"};
             }
             res.json(response);
         });
